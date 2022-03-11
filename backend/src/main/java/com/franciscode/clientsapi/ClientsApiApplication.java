9f6c4760e3e598e5fbe3f261123097aefc32f43d
Felipe Correia <felipedreamcorreia@hotmail.com>
2022-03-11 00:41:53 -0300

    feature: Client entity created

--- backend/src/main/java/com/franciscode/clientsapi/ClientsApiApplication.java
+++ backend/src/main/java/com/franciscode/clientsapi/ClientsApiApplication.java
@@ -1,4 +1,4 @@
-package com.franciscode.clientsApi;
+package com.franciscode.clientsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
